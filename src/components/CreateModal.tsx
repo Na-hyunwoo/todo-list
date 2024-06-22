58664af76bb84f80dae9cd5c789c652fe759bd41
hyunwoo <Na-hyunwoo@users.noreply.github.com>
2024-06-22 23:18:58 +0900

    feat: dim 추가

--- src/components/CreateModal.tsx
+++ src/components/CreateModal.tsx
@@ -16,7 +16,12 @@ function CreateModal({ id, children }: CreateModalType) {
     return null;
   }
 
-  return createPortal(children, document.getElementById(MODAL_KEY.CONTAINER)!);
+  return createPortal(
+    <div className="fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0">
+      {children}
+    </div>,
+    document.getElementById(MODAL_KEY.CONTAINER)!
+  );
 }
 
 export default CreateModal;
