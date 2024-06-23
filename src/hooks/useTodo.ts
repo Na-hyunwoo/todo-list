c1096a1987d9326b2b3f3e4bede42838a9f77b80
hyunwoo <Na-hyunwoo@users.noreply.github.com>
2024-06-24 08:19:04 +0900

    feat: 체크 버튼 추가

--- src/hooks/useTodo.ts
+++ src/hooks/useTodo.ts
@@ -4,21 +4,26 @@ import { create } from "zustand";
 type Todo = {
   id: string;
   content: string;
+  checked: boolean;
 };
+
+type PartialTodo = Partial<Omit<Todo, "id">> & { id: string };
 
 type UseTodo = {
   todos: Todo[];
   add: (content: string) => void;
   delete: (id: string) => void;
   reset: () => void;
-  modify: (id: string, content: string) => void;
+  modify: (todo: PartialTodo) => void;
 };
 
 const useTodo = create<UseTodo>((set) => ({
   todos: [],
-  add: (content: string) =>
-    set((state) => ({ todos: [...state.todos, { id: uuidv4(), content }] })),
-  delete: (id: string) =>
+  add: (content) =>
+    set((state) => ({
+      todos: [...state.todos, { id: uuidv4(), content, checked: false }],
+    })),
+  delete: (id) =>
     set((state) => ({
       todos: [...state.todos].filter(({ id: _id }) => id !== _id),
     })),
@@ -26,15 +31,21 @@ const useTodo = create<UseTodo>((set) => ({
     set(() => ({
       todos: [],
     })),
-  modify: (id: string, content: string) =>
+  modify: ({ id, content, checked }) =>
     set((state) => ({
-      todos: [...state.todos].map(({ id: _id, content: _content }) => {
-        if (id === _id) {
-          return { id, content };
-        } else {
-          return { id: _id, content: _content };
-        }
-      }),
+      todos: [...state.todos].map(
+        ({ id: _id, content: _content, checked: _checked }) => {
+          if (id === _id) {
+            return {
+              id,
+              content: content ?? _content,
+              checked: checked ?? _checked,
+            };
+          } else {
+            return { id: _id, content: _content, checked: _checked };
+          }
+        },
+      ),
     })),
 }));
 
--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,52 +1,65 @@
-import { useState } from "react";
 import useModal from "@/hooks/useModal";
 import Spacing from "@/components/Spacing";
 import { MODAL_KEY } from "@/utils/const";
 import AddTodoModal from "@/components/AddTodoModal";
 import CreateModal from "@/components/CreateModal";
-
-type Todo = {
-  key: string;
-  content: string;
-};
+import useTodo from "@/hooks/useTodo";
+import { ChangeEvent } from "react";
 
 function TodoList() {
   const [onOpen] = useModal((state) => [state.onOpen]);
 
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, modify, reset] = useTodo((state) => [
+    state.todos,
+    state.modify,
+    state.reset,
+  ]);
 
   const handleClickAdd = () => {
     onOpen(MODAL_KEY.ADD_TODO);
   };
 
-  const handleClickReset = () => {
-    setTodos([]);
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>, id: string) => {
+    modify({ id, checked: e.target.checked });
   };
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center my-8">
-        <h1 className="text-4xl font-bold ">현우의 TODO 애플리케이션</h1>
+      <div className="my-8 flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold">현우의 TODO 애플리케이션</h1>
         <Spacing height={32} />
         <div className="flex flex-col items-center">
-          <div className="flex gap-10 justify-center">
+          <div className="flex justify-center gap-10">
             <button className="text-xl font-bold" onClick={handleClickAdd}>
               추가하기
             </button>
-            <button className="text-xl font-bold" onClick={handleClickReset}>
+            <button className="text-xl font-bold" onClick={reset}>
               초기화하기
             </button>
           </div>
 
           <Spacing height={32} />
 
-          <ul>
-            {todos.map(({ key, content }) => (
-              <li className="font-bold flex gap-4" key={key}>
-                <p>{content}</p>
+          {/**
+           * 1. 체크되면 content에 찍 그어짐
+           * 2. 수정하기는 커서 올려서 클릭하면, 그리고 밖에 클릭하면 사라짐
+           * 3. 삭제하기는 x 버튼으로 대체 ?
+           */}
+          <ul className="flex flex-col gap-2">
+            {todos.map(({ id, content, checked }) => (
+              <li className="flex font-bold" key={id}>
+                <input
+                  type="checkbox"
+                  checked={checked}
+                  className="w-5 cursor-pointer"
+                  onChange={(e) => handleChangeInput(e, id)}
+                />
+                <p className={`pl-2 pr-4 ${checked && "line-through"}`}>
+                  {content}
+                </p>
                 <div className="flex gap-2">
                   <button>수정하기</button>
-                  <button>저장하기</button>
+                  <button>삭제하기</button>
                 </div>
               </li>
             ))}
